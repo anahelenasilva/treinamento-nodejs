c494c4a22cc826e6c71a536ddc2db7348b12de97
Ana Helena <anahelenarp@hotmail.com>
2022-01-28 16:40:12 -0300

    feat: check if customer already exists

--- src/index.js
+++ src/index.js
@@ -16,6 +16,11 @@ app.post('/account', (request, response) => {
     const { cpf, name } = request.body
     const id = uuidV4()
 
+    const customerAlreadyExists = customers.some(customer => customer.cpf === cpf)
+    if (customerAlreadyExists) {
+        return response.status(400).json({ error: 'Customer already exists' })
+    }
+
     customers.push({
         cpf,
         name,
